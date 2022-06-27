f5079da8fbf4749e9aedff16c999b00d86300ee0
jdavis-appd <95317448+jdavis-appd@users.noreply.github.com>
2022-06-27 14:28:25 -0700

    ios plugin interface list
    added all the functions in the plugin list

--- ios/Plugin/ADEUMMobileCapacitorPluginPlugin.m
+++ ios/Plugin/ADEUMMobileCapacitorPluginPlugin.m
@@ -18,7 +18,26 @@ CAP_PLUGIN(ADEUMMobileCapacitorPluginPlugin, "ADEUMMobileCapacitorPlugin",
            CAP_PLUGIN_METHOD(removeUserData, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(takeScreenshot, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(beginCall, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(endCall, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(beginHttpRequest, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(reportDone, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withResponseCode, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withResponseContentLength, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withRequestContentLength, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withResponseHeaderFields, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withRequestHeaderFields, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withInstrumentationSource, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(withErrorMessage, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(getCorrelationHeaders, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startNextSession, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(unblockScreenshots, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(blockScreenshots, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(screenshotsBlocked, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(startSessionFrame, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(endSessionFrame, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(updateSessionFrameName, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getVersion, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(clear, CAPPluginReturnNone);
             //we don't declare load but if this doesn't auto init agent we may need to make our init it's own
             //method and then declare it
            //CAP_PLUGIN_METHOD(load, CAPPluginReturnPromise);
